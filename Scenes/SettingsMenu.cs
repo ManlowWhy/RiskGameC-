40e42fa80181884d92ee0acaa4a42713744f6576
ManlowWhy <olmansr2007@gmail.com>
2025-09-29 23:43:08 -0600

    Diseño y audio
    Se añadió fondo de menú y se configuro el sonido de fondo

--- Scenes/SettingsMenu.cs
+++ Scenes/SettingsMenu.cs
@@ -4,10 +4,26 @@ public partial class SettingsMenu : Control
 {
 	[Export] public NodePath BackButtonPath;
 	private Button _btnBack;
+	private HSlider _slider;
 
 	public override void _Ready()
 	{
+		// Botón Back
 		_btnBack = GetNode<Button>(BackButtonPath ?? "Center/VBox/BtnBack");
 		_btnBack.Pressed += () => GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
+
+		// Slider de volumen
+		_slider = GetNode<HSlider>("Center/VBox/Volumen");
+
+		int bus = AudioServer.GetBusIndex("Music"); // ⚡ bus de música
+		_slider.MinValue = -40;
+		_slider.MaxValue = 0;
+		_slider.Step = 1;
+		_slider.Value = AudioServer.GetBusVolumeDb(bus);
+
+		_slider.ValueChanged += (double v) =>
+		{
+			AudioServer.SetBusVolumeDb(bus, (float)v);
+		};
 	}
 }
